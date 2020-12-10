bcf9ac07a06d66518c89ad9a95dcbf8858374e13
Aearil <aearil@paranoici.org>
2020-12-10 02:11:05 +0100

    Clean up code structure and fix buffer overrun

--- vitreye.c
+++ vitreye.c
@@ -10,12 +10,23 @@
 #define FPS 30
 #define NB_RECTS 64
 
+typedef struct {
+	SDL_Rect outer;
+	SDL_Rect inner;
+} Tile;
+
+typedef struct {
+	Tile *tiles;
+	size_t len;
+} TileLayout;
+
 static SDL_Renderer *renderer = NULL;
 static SDL_Window *window = NULL;
-static SDL_Rect *tiles = NULL;
 static Vec2i *rects = NULL;
 static Vec2i winsize;
 static Vec2i winpos;
+static TileLayout layout = {NULL, 0};
+static SDL_Rect *bgrects, *fgrects;
 
 static void
 gentestrects(Vec2i **rects, size_t count, Vec2i min, Vec2i max)
@@ -25,7 +36,7 @@ gentestrects(Vec2i **rects, size_t count, Vec2i min, Vec2i max)
 	/* Generate the same pattern each time */
 	srand(1);
 
-	*rects = ecalloc(count, sizeof(**rects));
+	*rects = ecalloc(count, sizeof(SDL_Rect));
 	for (i = 0; i < count; ++i) {
 		(*rects)[i].x = (uint64_t)rand() * (max.x - min.x) / RAND_MAX + min.x;
 		(*rects)[i].y = (uint64_t)rand() * (max.y - min.y) / RAND_MAX + min.y;
@@ -53,6 +64,10 @@ init(void)
 	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
 
 	gentestrects(&rects, NB_RECTS, (Vec2i){100, 100}, (Vec2i){2048, 2048});
+	layout.len = NB_RECTS;
+	layout.tiles = ecalloc(layout.len, sizeof(Tile));
+	bgrects = ecalloc(layout.len, sizeof(SDL_Rect));
+	fgrects = ecalloc(layout.len, sizeof(SDL_Rect));
 
 	return 1;
 }
@@ -81,7 +96,8 @@ keyboard(SDL_Event *e)
 static int
 getfreecell(int *occupied, size_t len, int width, Vec2i format)
 {
-	int x, y, cx, cy, i;
+	size_t i;
+	int x, y, cx, cy;
 	int empty;
 
 	if (format.x > width)
@@ -91,6 +107,8 @@ getfreecell(int *occupied, size_t len, int width, Vec2i format)
 		x = i % width;
 		y = i / width;
 		if (x + format.x > width)
+			continue;
+		if (y + format.y > len / width)
 			continue;
 		empty = 1;
 		for (cx = x; cx < x + format.x && empty; cx++)
@@ -110,77 +128,100 @@ getfreecell(int *occupied, size_t len, int width, Vec2i format)
 }
 
 static void
-draw()
+gentileslayout(TileLayout *layout)
 {
 	const int Y_MARGIN = 10;
+	// FIXME There is no guarantee this is enough in the worst case
 	const int NB_CELLS = NB_RECTS + 16;
-	int x = 0, y = 0, i = 0;
+	size_t i;
+	int x = 0, y = 0;
 	int margin;
 	int nx, ny;
 	int *occupied;
 	SDL_Rect *r, *t;
 
-	SDL_RenderClear(renderer);
-
 	nx = winsize.x / TILE_W;
 	ny = winsize.y / TILE_H;
-	// TODO stop allocating once per frame
-	r = ecalloc(NB_RECTS, sizeof(SDL_Rect));
-	t = ecalloc(NB_RECTS, sizeof(SDL_Rect));
 	occupied = ecalloc(NB_CELLS, sizeof(int));
 	margin = (winsize.x % TILE_W) / nx;
 
-	for (i = 0; i < MIN(NB_RECTS, nx * ny); ++i) {
+	for (i = 0; i < layout->len; ++i) {
 		float ratiodiff = TILE_H / (float)TILE_W - rects[i].y / (float)rects[i].x;
 		float ratio2;
 		int j;
 		Vec2i format;
 
-		if (ratiodiff > 0.4) {
+		if (ratiodiff > 0.4 && nx > 1) {
 			format = (Vec2i){2, 1};
 		} else if (ratiodiff < -0.4) {
 			format = (Vec2i){1, 2};
 		} else {
 			format = (Vec2i){1, 1};
 		}
+		/* Use a greedy algorithm to layout the tiles. We're  hoping for enough
+		 * variety in the tiles to fill the holes left by high aspect ratio tiles */
 		j = getfreecell(occupied, NB_CELLS, nx, format);
-		//printf("Free cell, j = %d\n", j);
 		if (j < 0)
 			break;
 		x = j % nx;
 		y = j / nx;
 
-		r[i].w = TILE_W * format.x + margin * (format.x - 1);
-		r[i].h = TILE_H * format.y + Y_MARGIN * (format.y - 1);
-		r[i].x = x * (TILE_W + margin) + margin / 2;
-		r[i].y = y * (TILE_H + Y_MARGIN) + Y_MARGIN / 2;
+		r = &layout->tiles[i].outer;
+		t = &layout->tiles[i].inner;
+		r->w = TILE_W * format.x + margin * (format.x - 1);
+		r->h = TILE_H * format.y + Y_MARGIN * (format.y - 1);
+		r->x = x * (TILE_W + margin) + margin / 2;
+		r->y = y * (TILE_H + Y_MARGIN) + Y_MARGIN / 2;
 
-		ratio2 = r[i].h / (float)r[i].w - rects[i].y / (float)rects[i].x;
+		ratio2 = r->h / (float)r->w - rects[i].y / (float)rects[i].x;
 		if (ratio2 > 0) {
-			t[i].w = r[i].w;
-			t[i].h = r[i].w * rects[i].y / rects[i].x;
-			t[i].x = r[i].x;
-			t[i].y = r[i].y + (r[i].h - t[i].h) / 2;
+			t->w = r->w;
+			t->h = r->w * rects[i].y / rects[i].x;
+			t->x = r->x;
+			t->y = r->y + (r->h - t->h) / 2;
 		} else {
-			t[i].w = r[i].h * rects[i].x / rects[i].y;
-			t[i].h = r[i].h;
-			t[i].x = r[i].x + (r[i].w - t[i].w) / 2;
-			t[i].y = r[i].y;
+			t->w = r->h * rects[i].x / rects[i].y;
+			t->h = r->h;
+			t->x = r->x + (r->w - t->w) / 2;
+			t->y = r->y;
+		}
+	}
+
+	FREE(occupied);
+}
+
+static void
+resize()
+{
+	gentileslayout(&layout);
+}
+
+static void
+draw()
+{
+	size_t i;
+
+	SDL_RenderClear(renderer);
+
+	for (i = 0; i < layout.len; i++) {
+		SDL_Rect r = layout.tiles[i].outer;
+		if (r.x > winsize.x && r.y > winsize.y) {
+			bgrects[i] = (SDL_Rect){};
+			fgrects[i] = (SDL_Rect){};
+		} else {
+			bgrects[i] = r;
+			fgrects[i] = layout.tiles[i].inner;
 		}
 	}
 
 	SDL_SetRenderDrawColor(renderer, 0x33, 0x33, 0x33, 0xff);
-	SDL_RenderFillRects(renderer, r, MIN(NB_RECTS, nx * ny));
+	SDL_RenderFillRects(renderer, bgrects, layout.len);
 
 	SDL_SetRenderDrawColor(renderer, 0xee, 0xee, 0xee, 0xff);
-	SDL_RenderFillRects(renderer, t, MIN(NB_RECTS, nx * ny));
+	SDL_RenderFillRects(renderer, fgrects, layout.len);
 
 	SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xff);
 	SDL_RenderPresent(renderer);
-
-	FREE(r);
-	FREE(t);
-	FREE(occupied);
 }
 
 int
@@ -206,6 +247,7 @@ main(int argc, char* argv[])
 					e.window.event == SDL_WINDOWEVENT_RESIZED)) {
 				SDL_GetWindowSize(window, &winsize.x, &winsize.y);
 				SDL_GetWindowPosition(window, &winpos.x, &winpos.y);
+				resize();
 			}
 		}
 
